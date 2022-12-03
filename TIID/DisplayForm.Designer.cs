69c5879d54694a53a9b5936da2e2df0a584567b4
Antonio Mohorić <mohoricantonio@gmail.com>
2022-12-03 11:21:46 +0100

    Finding my coordinates
    doesn't word

--- TIID/DisplayForm.Designer.cs
+++ TIID/DisplayForm.Designer.cs
@@ -29,27 +29,19 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Controls.Add(this.map);
             this.panel1.Location = new System.Drawing.Point(360, 153);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(648, 569);
             this.panel1.TabIndex = 0;
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(648, 569);
-            this.webBrowser1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -58,12 +50,46 @@
             this.panel2.Size = new System.Drawing.Size(355, 238);
             this.panel2.TabIndex = 1;
             // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(4, 3);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(641, 566);
+            this.map.TabIndex = 1;
+            this.map.Zoom = 0D;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 2;
+            // 
             // DisplayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
             this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "DisplayForm";
@@ -71,14 +97,16 @@
             this.Load += new System.EventHandler(this.DisplayForm_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Panel panel2;
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- TIID/DisplayForm.cs
+++ TIID/DisplayForm.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using GMap.NET.MapProviders;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Device.Location;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +15,9 @@ namespace TIID
 {
     public partial class DisplayForm : Form
     {
+        public double currentLat;
+        public double currentLon;
+        private GeoCoordinateWatcher watcher;
         public DisplayForm()
         {
             InitializeComponent();
@@ -19,10 +25,44 @@ namespace TIID
 
         private void DisplayForm_Load(object sender, EventArgs e)
         {
-            StringBuilder lokacija = new StringBuilder();
-            lokacija.Append("http://google.com/maps?q=");
-            string trenutnaLokacija = "";
+            watcher = new GeoCoordinateWatcher();
+            watcher.Start();
+            //watcher.PositionChanged += GetCurrentLocation;
+            GetCurrentLocation();
+            //map.MapProvider = GMapProviders.GoogleMap;
+            
+            map.MinZoom = 5;
+            map.MaxZoom = 100;
+            map.Zoom = 10;
+            //map.Position = new GMap.NET.PointLatLng(currentLon, currentLat);
+           
 
+        }
+        private void GetCurrentLocation()
+        {
+            /* try
+             {
+                 if(e.Status == GeoPositionStatus.Ready)
+                 {
+                     if (watcher.Position.Location.IsUnknown)
+                     {
+                         currentLat = 0;
+                         currentLon = 0;
+                     }
+                     else
+                     {
+                         currentLat = watcher.Position.Location.Latitude;
+                         currentLon = watcher.Position.Location.Longitude;
+                     }
+                 }
+             }
+             catch (Exception)
+             {
+
+                 throw;
+             }
+             MessageBox.Show(currentLat.ToString() + " " + currentLon.ToString());*/
+            MessageBox.Show(watcher.Position.Location.Longitude + " " + watcher.Position.Location.Latitude);
         }
     }
 }
